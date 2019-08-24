9da1358789fbfeb1c091e1cf03d0c847c1746d12
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-08-24 18:29:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,22 +11,38 @@ class ApplicationController < Sinatra::Base
 	get "/" do
 		erb :index
 	end
+	#renders an index.erb file with links to signup or login.
 
 	get "/signup" do
 		erb :signup
 	end
+# renders a form to create a new user. The form includes fields for username and password.
 
 	post "/signup" do
-		#your code here!
+	user = User.new(:username => params[:username], :password => params[:password])
+	#make a new instance of our user class with a username and password from params. Note that even though our database has a #column called password_digest, we still access the attribute of password. This is given to us by has_secure_password
+	 if user.save
+    redirect "/login"
+  else
+    redirect "/failure"
+  end
+  
 	end
 
 	get "/login" do
 		erb :login
 	end
+#renders a form for logging in.
 
 	post "/login" do
-	  ghfghgf
-		#your code here!
+user = User.find_by(:username => params[:username])
+ 
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/success"
+  else
+    redirect "/failure"
+  end
 	end
 
 	get "/success" do
@@ -40,11 +56,13 @@ class ApplicationController < Sinatra::Base
 	get "/failure" do
 		erb :failure
 	end
+	#renders a failure.erb page. This will be accessed if there is an error logging in or signing up.
 
 	get "/logout" do
 		session.clear
 		redirect "/"
 	end
+	#clears the session data and redirects to the homepage.
 
 	helpers do
 		def logged_in?
@@ -57,3 +75,5 @@ class ApplicationController < Sinatra::Base
 	end
 
 end
+
+#BCrypt will store a salted, hashed version of our users' passwords in our database in a column called password_digest. Essentially, once a password is salted and hashed, there is no way for anyone to decode it. This method requires that hackers use a 'brute force' approach to gain access to someone's account –– still possible, but more difficult.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+	 has_secure_password
+	#has_secure_password is being called just like a normal ruby method. It works in conjunction with a gem called bcrypt and gives #us all of those abilities in a secure way that doesn't actually store the plain text password in the database
 	
+	#we told Ruby to add an authenticate method to our class (invisibly!) when the program runs. While we, as programmers can't see it, it will be there.
 end
